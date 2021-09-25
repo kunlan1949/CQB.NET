8980c7e0840e20cce1f02de5545401a03dc13877
pan <nautiloidea.pan@brstar.com.cn>
2021-09-25 14:04:29 +0800

    添加项目文件。

---
+++ CQB.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CQB.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
